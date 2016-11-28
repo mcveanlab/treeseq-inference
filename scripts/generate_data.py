1917589c98086e949bde2a38761abd2035f1aef8
Yan Wong <yan@yanwong.me>
2016-11-28 12:58:43 +0000

    different file extension for fastARG input ('.haps')

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -1,15 +1,18 @@
+#!/usr/bin/env python3
+
 """
 Generate data to run fastARG on.
 """
 import os
 
 import numpy as np
-
+import sys
+sys.path.insert(0, '../msprime/') # look at the local version of ms
 import msprime
 
 def main():
     sample_sizes = list(map(int, np.linspace(500, 5000, 10)))
-    tmpdir = "../"
+    tmpdir = "./"
     for n in sample_sizes:
         ts = msprime.simulate(
             sample_size=n, Ne=1e4, mutation_rate=2e-8, recombination_rate=2e-8,
@@ -20,7 +23,7 @@ def main():
         outfile = os.path.join(tmpdir, "{}.hdf5".format(n))
         ts.dump(outfile)
 
-        fastarg_file = os.path.join(tmpdir, "{}.fastarg".format(n))
+        fastarg_file = os.path.join(tmpdir, "{}.haps".format(n))
         with open(fastarg_file, "w") as f:
             for j, v in enumerate(ts.variants(as_bytes=True)):
                 print(j, v.genotypes.decode(), sep="\t", file=f)
