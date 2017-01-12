aab53ea923e40159a0cdbee676796ce9fa93c8bd
Jerome Kelleher <jk@well.ox.ac.uk>
2017-01-12 16:24:04 +0000

    Removed unused packages, updated requirements.

--- src/plots.py
+++ src/plots.py
@@ -18,7 +18,6 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as pyplot
-import seaborn as sns
 import pandas as pd
 
 # import the local copy of msprime in preference to the global one
