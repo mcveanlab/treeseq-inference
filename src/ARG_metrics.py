13ea375d8d24ade657ea23cc91f88682796edc4c
Yan Wong <yan@yanwong.me>
2017-02-05 23:21:12 +0000

    allow variant positions to be used from python

--- src/ARG_metrics.py
+++ src/ARG_metrics.py
@@ -1,6 +1,7 @@
 import logging
 import pandas as pd
 import rpy2.robjects as robjects
+import rpy2.rinterface as rinterface
 from rpy2.robjects.packages import importr
 
 ape=importr("ape")
@@ -13,7 +14,7 @@ ARGmetrics = importr("ARGmetrics")
 #Open R and set the working dir to treeseq-inference, then do
 # > install("ARGmetrics")
 
-def get_ARG_metrics(true_nexus_fn=None, threads=1, **inferred_nexus_fns):
+def get_ARG_metrics(true_nexus_fn=None, threads=1, variant_positions = None, **inferred_nexus_fns):
     """
     The complicated thing here is the inferred_nexus_fns parameters.
     Each should contain a dictionary with a 'nexus' item pointing to 
@@ -44,6 +45,7 @@ def get_ARG_metrics(true_nexus_fn=None, threads=1, **inferred_nexus_fns):
         true_nexus_fn, threads, {k: len(v['nexus']) for k,v in inferred_nexus_fns.items()}))
     # load the true_nexus into the R session (but don't convert it to a python obj)
     orig_tree = ape.read_nexus(true_nexus_fn, force_multi=True)
+    var_pos = variant_positions or rinterface.NULL
     weights = 1
     metrics = None
     for tool, metric_params in inferred_nexus_fns.items():
@@ -57,16 +59,15 @@ def get_ARG_metrics(true_nexus_fn=None, threads=1, **inferred_nexus_fns):
                 logging.debug("calculating tree metrics to compare binarised '{}' vs '{}'.".format(
                     true_nexus_fn, nexus_files))
                 if seed:
-                
-                    m = ARGmetrics.genome_trees_dist_forcebin_b(orig_tree, nexus_files, 
-                         replicates = break_binary_reps, seed = int(seed), threads=threads)
+                    m = ARGmetrics.genome_trees_dist_forcebin_b(orig_tree, nexus_files, seed = int(seed),
+                         variant_positions=var_pos, replicates = break_binary_reps, threads=threads)
                 else:
                     m = ARGmetrics.genome_trees_dist_forcebin_b(orig_tree, nexus_files, 
-                         replicates = break_binary_reps, threads=threads)
+                         variant_positions=var_pos, replicates = break_binary_reps, threads=threads)
             except:
                 logging.debug("calculating tree metrics to compare '{}' vs '{}'.".format(
                     true_nexus_fn, nexus_files))
-                m = ARGmetrics.genome_trees_dist(orig_tree, nexus_files)
+                m = ARGmetrics.genome_trees_dist(orig_tree, nexus_files, variant_positions=var_pos)
         else:
             if len(nexus_files) == 0:
                 logging.debug("No inference files give to compare")
@@ -74,8 +75,30 @@ def get_ARG_metrics(true_nexus_fn=None, threads=1, **inferred_nexus_fns):
             else:
                 logging.debug("calculating tree metrics to compare '{}' vs {} other files.".format(
                     true_nexus_fn, len(nexus_files)))
-                m = ARGmetrics.genome_trees_dist_multi(orig_tree, nexus_files, weights=1)
+                m = ARGmetrics.genome_trees_dist_multi(orig_tree, nexus_files, 
+                    variant_positions=var_pos, weights=1)
         if metrics is None:
             metrics = pd.DataFrame(columns = m.names)
         metrics.loc[tool, tuple(m.names)] = tuple(m)
     return metrics
+
+if __name__ == "__main__":
+    """Test whether we get sensible metrics for a tree sequence"""
+    import tempfile
+    
+    ts = [{3:"(1,(2,3));", 4:"((1,2),3);"},
+          {4:"((1,2),3);"}]
+    #this should give an RF distance of 2 for the 1st tree and 0 for the second
+    with tempfile.NamedTemporaryFile("w+") as nex1, \
+        tempfile.NamedTemporaryFile("w+") as nex2:
+        for fh, trees in zip([nex1, nex2], ts):
+            print("#NEXUS\nBEGIN TREES;",file=fh)
+            print("TRANSLATE\n{};".format(",\n".join(["{} {}".format(i,i) for i in [1,2,3]])), file = fh)
+            for endpoint in sorted(trees.keys()):
+                print("TREE " + str(endpoint) + " = " + trees[endpoint], file=fh) 
+            print("END;", file = fh)
+            fh.flush()
+        #Genome average should be (2*3 + 0*1) /4 = 6/4 = 1.5
+        print(get_ARG_metrics(nex1.name, test={'nexus':nex2.name}))
+        #Positional average for should be (2*2 + 0*2) /5 = 4/5 = 0.8
+        print(get_ARG_metrics(nex1.name, variant_positions=[0, 2.9999,3,3.5, 4],test={'nexus':nex2.name}))
