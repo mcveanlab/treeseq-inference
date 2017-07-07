380232f027a123619cc56314403739444722ad72
Yan Wong <yan@yanwong.me>
2017-07-07 12:46:30 +0100

    output vcf array to hdf5 file

--- scripts/vcf2tsinfer.py
+++ scripts/vcf2tsinfer.py
@@ -2,14 +2,23 @@
 """
 Take a vcf file with ancestral information (e.g. from 1000 genomes phase 1, such as
 ALL.chr22.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf.gz
-and convert it to a huge numpy array. Also output the variant positions and the sample names. Use as
+and convert it to the huge samples X sites array used in tsinfer. Store this
+in the hdf5 output file, and also store the variant positions and the sample names.
 
-vcf2tsinfer.py 1000G_chr22.vcf.gz outputarrays.npz
+vcf2tsinfer.py 1000G_chr22.vcf.gz outputarrays.hdf5
+
+NB - a slightly odd observation: all the alleles for which the ancestral state is not present in the dataset are SNPs
+
+read using
+with h5py.File(filename, 'r') as f:
+    print(f['data']['variants'][()])
 """
 import sys
-
+import h5py
 import pysam
 import numpy as np
+
+#To do - use argparse module 
 
 vcf_in = pysam.VariantFile(sys.argv[1])
 
@@ -19,34 +28,44 @@ for sample_name in vcf_in.header.samples:
     for suffix in ('a','b'):
         rows[sample_name+suffix]=row
         row+=1
-cols=0
-positions = []
-data = []
+position = []
+n_vars = 400000 #fill in from https://groups.google.com/forum/#!topic/pysam-user-group/-Obv5ggp9tk
+sites_by_samples = np.zeros((n_vars, len(rows)), dtype="i1")
 output_status_bases = 1e6 #how many bases between status outputs
 curr_output_after = output_status_bases
 
 for rec in vcf_in.fetch():
+    allele_count[len(rec.alleles)] += 1
     #restrict to cases where ancestral state contains only letters ATCG
     # i.e. where the ancestral state is certain (see ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase1/analysis_results/supporting/ancestral_alignments/human_ancestor_GRCh37_e59.README
     if "AA" in rec.info and all(letter in "ATCG" for letter in rec.info["AA"]):
-        allele_count[len(rec.alleles)] += 1
         #only use biallelic variants
         if len(rec.alleles) == 2:
             if rec.info["AA"] not in rec.alleles:
-                print("Ancestral state {} not in allele list ({})".format(\
-                    rec.info["AA"], rec.alleles))
+                print("Ancestral state {} not in allele list ({}) for position {}".format(\
+                    rec.info["AA"], rec.alleles, rec.pos))
             else:
-                column = np.zeros((len(rows),), dtype=np.bool)
+                omit=False
+                column = np.zeros((len(rows),), dtype="i1")
                 for label, samp in rec.samples.items():
                     for i,suffix in enumerate(('a','b')):
                         #print("allele {} (pos {}, sample {}, ancestral state {}, alleles {})".format( \
                         #    data.alleles[i], rec.pos, label+suffix, rec.info["AA"], rec.alleles))
+                        if samp.alleles[i] not in rec.alleles:
+                            print("@ position {}, sample allele {} is not in {} - could be missing data. Omitting this row".format(rec.pos, samp.alleles[i], rec.alleles))
+                            omit=True
                         column[rows[label+suffix]] = samp.alleles[i]!=rec.info["AA"]
-                positions.append(rec.pos)
-                data.append(column)
+                if not omit:
+                    sites_by_samples[len(position)]=column
+                    position.append(rec.pos)
         if rec.pos > curr_output_after:
             print("@ base position {} Mb (alleles per site: {})".format(rec.pos/1e6, allele_count))
             while curr_output_after < rec.pos:
                 curr_output_after += output_status_bases
-np.savez(sys.argv[2], data=np.array(data), samples=np.array(sorted(rows, key= rows.get),dtype='|U{}'.format(max([len(n) for n in rows.keys()]))), positions=np.array(positions))
-print("Saved {} biallelic loci for {} samples into {}".format(len(positions), len(rows), sys.argv[2]))
+
+with h5py.File(sys.argv[2], "w") as f:
+    g = f.create_group("data")
+    g.create_dataset("position", data=position)
+    g.create_dataset("samples", data=[s.encode() for s in sorted(rows, key= rows.get)])
+    g.create_dataset("variants", data=np.transpose(sites_by_samples[:len(position)]))
+print("Saved {} biallelic loci for {} samples into {}".format(len(position), len(rows), sys.argv[2]))
