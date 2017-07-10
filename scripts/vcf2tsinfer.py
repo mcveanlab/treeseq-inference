5a30952b7212f2b4c7257405890551fa0ce1520f
Yan Wong <yan@yanwong.me>
2017-07-10 13:32:41 +0100

    gradually expand the numpy array store
    can’t get # variants from VCF easily

--- scripts/vcf2tsinfer.py
+++ scripts/vcf2tsinfer.py
@@ -23,22 +23,11 @@ import pysam
 
 vcf_in = pysam.VariantFile(sys.argv[1])
 
-allele_count = {}
-rows, row = {}, 0
-for sample_name in vcf_in.header.samples:
-    for suffix in ('a','b'):
-        rows[sample_name+suffix]=row
-        row+=1
-position = collections.OrderedDict()
-n_vars = 400000 #fill in from https://groups.google.com/forum/#!topic/pysam-user-group/-Obv5ggp9tk
-sites_by_samples = np.zeros((n_vars, len(rows)), dtype="i1")
-output_freq_variants = 1e3 #output status after multiples of this many variants read
-
-def process_variant(rec):
+def process_variant(rec, data):
     """
     return True only when
     """
-    allele_count[len(rec.alleles)] = allele_count.get(len(rec.alleles),0) + 1
+    data['allele_count'][len(rec.alleles)] = data['allele_count'].get(len(rec.alleles),0) + 1
     #restrict to cases where ancestral state contains only letters ATCG
     # i.e. where the ancestral state is certain (see ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase1/analysis_results/supporting/ancestral_alignments/human_ancestor_GRCh37_e59.README
     if "AA" in rec.info and all(letter in "ATCG" for letter in rec.info["AA"]):
@@ -67,13 +56,13 @@ def process_variant(rec):
                     allele_start=0
                     pos = rec.pos
 
-                if pos in position:                    
+                if pos in data['position']:                    
                     print("More than one set of variants at position {}. ".format(pos))
                     print("Previous was {}. Omitting a subsequent duplicate (id {})".format(
-                        position[pos], {rec.id:rec.alleles}))
+                        data['position'][pos], {rec.id:rec.alleles}))
                     return False
 
-                column = np.zeros((len(rows),), dtype="i1")
+                column = np.zeros((len(data['rows']),), dtype="i1")
                 for label, samp in rec.samples.items():
                     for i,suffix in enumerate(('a','b')):
                         #print("allele {} (pos {}, sample {}, ancestral state {}, alleles {})".format( \
@@ -82,23 +71,37 @@ def process_variant(rec):
                             print("@ position {}{}, sample allele {} is not in {} - could be missing data. Omitting this row".format(
                                 rec.pos, "+{}".format(allele_start) if allele_start else "", samp.alleles[i], rec.alleles))
                             return False
-                        column[rows[label+suffix]] = samp.alleles[i][allele_start:]!=rec.info["AA"][allele_start:]
-                
-                sites_by_samples[len(position)]=column
-                position[pos]={rec.id:rec.alleles}
+                        column[data['rows'][label+suffix]] = samp.alleles[i][allele_start:]!=rec.info["AA"][allele_start:]
+                if data['sites_by_samples'].shape[0] <= len(data['position']):
+                    #need more storage
+                    data['sites_by_samples'] = data['sites_by_samples'].append((data['extend_amount'], len(data['rows'])), dtype="i1")
+                data['sites_by_samples'][len(data['position'])]=column
+                data['position'][pos]={rec.id:rec.alleles}
                 return True
     return False
 
+store={}
+store['allele_count'] = {}
+store['rows'], row = {}, 0
+for sample_name in vcf_in.header.samples:
+    for suffix in ('a','b'):
+        store['rows'][sample_name+suffix]=row
+        row+=1
+store['position'] = collections.OrderedDict()
+store['extend_amount'] = 10000 #numpy storage array will be extended by this much at a time
+store['sites_by_samples'] = np.zeros((store['extend_amount'], len(store['rows'])), dtype="i1")
+output_freq_variants = 1e3 #output status after multiples of this many variants read
+
 for j, variant in enumerate(vcf_in.fetch()):
-    process_variant(variant)
+    process_variant(variant, store)
     if j % output_freq_variants == 0:
         print("{} variants read ({} saved). Base position {} Mb (alleles per site: {})".format(
-            j+1, len(position), variant.pos/1e6, [(k, allele_count[k]) for k in sorted(allele_count.keys())]), 
+            j+1, len(store['position']), variant.pos/1e6, [(k, store['allele_count'][k]) for k in sorted(store['allele_count'].keys())]), 
             flush=True)
 
 with h5py.File(sys.argv[2], "w") as f:
     g = f.create_group("data")
     g.create_dataset("position", data=position.keys())
-    g.create_dataset("samples", data=[s.encode() for s in sorted(rows, key= rows.get)])
-    g.create_dataset("variants", data=np.transpose(sites_by_samples[:len(position)]))
+    g.create_dataset("samples", data=[s.encode() for s in sorted(store['rows'], key= rows.get)])
+    g.create_dataset("variants", data=np.transpose(sites_by_samples[:len(store['position'])]))
 print("Saved {} biallelic loci for {} samples into {}".format(len(position), len(rows), sys.argv[2]))
