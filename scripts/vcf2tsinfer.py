bb682fc8681a5b6b14a3c08387f20a20efeeb408
Yan Wong <yan@yanwong.me>
2017-07-10 14:49:40 +0100

    omit all dups
    And refactor code to pass out vector to be used

--- scripts/vcf2tsinfer.py
+++ scripts/vcf2tsinfer.py
@@ -23,11 +23,10 @@ import pysam
 
 vcf_in = pysam.VariantFile(sys.argv[1])
 
-def process_variant(rec, data):
+def process_variant(rec, rows, site_data):
     """
-    return True only when
+    return the true position of this variant, or None if we wish to omit
     """
-    data['allele_count'][len(rec.alleles)] = data['allele_count'].get(len(rec.alleles),0) + 1
     #restrict to cases where ancestral state contains only letters ATCG
     # i.e. where the ancestral state is certain (see ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase1/analysis_results/supporting/ancestral_alignments/human_ancestor_GRCh37_e59.README
     if "AA" in rec.info and all(letter in "ATCG" for letter in rec.info["AA"]):
@@ -56,52 +55,63 @@ def process_variant(rec, data):
                     allele_start=0
                     pos = rec.pos
 
-                if pos in data['position']:                    
-                    print("More than one set of variants at position {}. ".format(pos))
-                    print("Previous was {}. Omitting a subsequent duplicate (id {})".format(
-                        data['position'][pos], {rec.id:rec.alleles}))
-                    return False
-
-                column = np.zeros((len(data['rows']),), dtype="i1")
                 for label, samp in rec.samples.items():
                     for i,suffix in enumerate(('a','b')):
                         #print("allele {} (pos {}, sample {}, ancestral state {}, alleles {})".format( \
                         #    rec.alleles[i], rec.pos, label+suffix, rec.info["AA"], rec.alleles))
                         if samp.alleles[i] not in rec.alleles:
-                            print("@ position {}{}, sample allele {} is not in {} - could be missing data. Omitting this row".format(
+                            print("@ position {}{}, sample allele {} is not in {} - could be missing data. Omitting this site".format(
                                 rec.pos, "+{}".format(allele_start) if allele_start else "", samp.alleles[i], rec.alleles))
-                            return False
-                        column[data['rows'][label+suffix]] = samp.alleles[i][allele_start:]!=rec.info["AA"][allele_start:]
-                if data['sites_by_samples'].shape[0] <= len(data['position']):
-                    #need more storage
-                    data['sites_by_samples'] = np.append(data['sites_by_samples'], np.zeros((data['extend_amount'], len(data['rows'])), dtype="i1"), axis=0)
-                data['sites_by_samples'][len(data['position'])]=column
-                data['position'][pos]={rec.id:rec.alleles}
-                return True
-    return False
+                            return None
+                        site_data[rows[label+suffix]] = samp.alleles[i][allele_start:]!=rec.info["AA"][allele_start:]
+                return pos
+    return None
 
 store={}
-store['allele_count'] = {}
-store['rows'], row = {}, 0
+allele_count = {}
+rows, row = {}, 0
 for sample_name in vcf_in.header.samples:
     for suffix in ('a','b'):
-        store['rows'][sample_name+suffix]=row
+        rows[sample_name+suffix]=row
         row+=1
-store['position'] = collections.OrderedDict()
-store['extend_amount'] = 10000 #numpy storage array will be extended by this much at a time
-store['sites_by_samples'] = np.zeros((store['extend_amount'], len(store['rows'])), dtype="i1")
+position = collections.OrderedDict()
+extend_amount = 10000 #numpy storage array will be extended by this much at a time
+sites_by_samples = np.zeros((extend_amount, len(rows)), dtype="i1")
 output_freq_variants = 1e3 #output status after multiples of this many variants read
+locus_data = np.zeros((len(rows),), dtype="i1")
 
 for j, variant in enumerate(vcf_in.fetch()):
-    process_variant(variant, store)
+    #keep track of allele numbers
+    allele_count[len(variant.alleles)] = allele_count.get(len(variant.alleles),0) + 1
+    #check if we now need more storage
+    if sites_by_samples.shape[0] <= len(position):
+        sites_by_samples = np.append(sites_by_samples, np.zeros((extend_amount, len(rows)), dtype="i1"), axis=0)
+
+    previous_position = -1
+    actual_position = process_variant(variant, rows, locus_data)
+    if actual_position is not None:
+        if actual_position in position or actual_position == previous_position:
+            print("Trying to store more than one set of variants at position {}. ".format(actual_position))
+            print("Previous was {}, this is {}.".format(
+                position.get(actual_position, prev_data), {variant.id:variant.alleles}))
+            if actual_position in position:
+                previous_position, prev_data = position.popitem()
+                print("Deleting original at postion {} as well as duplicates.".format(previous_position))
+        else:
+            sites_by_samples[len(position),:]=locus_data
+            position[actual_position]={variant.id: variant.alleles}
+            previous_postition=actual_position
+
+    
+        
     if j % output_freq_variants == 0:
         print("{} variants read ({} saved). Base position {} Mb (alleles per site: {})".format(
-            j+1, len(store['position']), variant.pos/1e6, [(k, store['allele_count'][k]) for k in sorted(store['allele_count'].keys())]), 
+            j+1, len(position), variant.pos/1e6, [(k, allele_count[k]) for k in sorted(allele_count.keys())]), 
             flush=True)
 
 with h5py.File(sys.argv[2], "w") as f:
     g = f.create_group("data")
     g.create_dataset("position", data=position.keys())
-    g.create_dataset("samples", data=[s.encode() for s in sorted(store['rows'], key= rows.get)])
-    g.create_dataset("variants", data=np.transpose(sites_by_samples[:len(store['position'])]))
+    g.create_dataset("samples", data=[s.encode() for s in sorted(rows, key= rows.get)])
+    g.create_dataset("variants", data=np.transpose(sites_by_samples[:len(position)]))
 print("Saved {} biallelic loci for {} samples into {}".format(len(position), len(rows), sys.argv[2]))
