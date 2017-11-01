adc61c458e0701167ad229031c22e92e00f0f9ad
Yan Wong <yan@yanwong.me>
2017-11-01 09:16:12 +0000

    add comments

--- scripts/SFS.py
+++ scripts/SFS.py
@@ -2,37 +2,51 @@ import msprime
 import collections
 import math
 
-recombination_rate=1e-8
-mutation_rate=1e-8
-ts = msprime.load("../ftprime/sweepfile0.8.hdf5")
-
 check_method = True
 
+recombination_rate=1e-8
+mutation_rate=1e-8
+ts = msprime.simulate(10000, 
+    Ne=10000, length=1000000, recombination_rate=recombination_rate, mutation_rate=mutation_rate, 
+    random_seed=6)
 trees = ts.trees(leaf_counts=True)
 diffs = ts.edge_diffs()
-
 SFS = collections.Counter()
+
+#first iteration introduces all the edges
+diff = next(diffs)
+tree = next(trees)
+for node in tree.nodes():
+    try:
+        SFS[tree.get_num_leaves(node)] += tree.get_branch_length(node)
+    except ValueError:
+        #can ignore root node (no branch length, mutations above it are fixed anyway) but not other nodes
+        if tree.get_parent(node) != msprime.NULL_NODE:
+            raise
+            
 for i, ((start, end), out_edges, in_edges) in enumerate(diffs):
     try:
-        if i:
-            check_nodes = set([edge.child for edge in out_edges] + [edge.parent for edge in in_edges])
-            removed_nodes = set()
-            for node in check_nodes:
-                #should be no out_edges at the start
-                #need to go up from this node and modify
-                while node not in removed_nodes and node != msprime.NULL_NODE:
-                    removed_nodes.add(node)
-                    node = tree.get_parent(node)
-            for node in removed_nodes:
-                try:
-                    SFS[tree.get_num_leaves(node)] -= tree.get_branch_length(node)
-                except ValueError:
-                    if tree.get_parent(node) == msprime.NULL_NODE:
-                        #the root node will not have a branch length, and can be ignored anyway
-                        pass
-                    else:
-                        raise
+        #REMOVE OUT EDGES: must check out nodes + ancestors *and* ancestors of in_nodes
+        #as an in node parent with >2 children will not appear in out_nodes 
+        check_nodes = set([edge.child for edge in out_edges] + [edge.parent for edge in in_edges])
+        removed_nodes = set()
+        for node in check_nodes:
+            #should be no out_edges at the start
+            #need to go up from this node and modify
+            while node not in removed_nodes and node != msprime.NULL_NODE:
+                removed_nodes.add(node)
+                node = tree.get_parent(node)
+        for node in removed_nodes:
+            try:
+                SFS[tree.get_num_leaves(node)] -= tree.get_branch_length(node)
+            except ValueError:
+                #can ignore root node (no branch length, mutations above it are fixed anyway) but not other nodes
+                if tree.get_parent(node) != msprime.NULL_NODE:
+                    raise
+        
         tree = next(trees)
+        #ADD IN EDGES: must check out nodes + ancestors *and* ancestors of out_nodes
+        #as an out node parent with >2 children will not appear in in_nodes 
         check_nodes = set([edge.child for edge in in_edges] + [edge.parent for edge in out_edges])
         added_nodes = set()
         for node in check_nodes:
@@ -40,14 +54,12 @@ for i, ((start, end), out_edges, in_edges) in enumerate(diffs):
             while node not in added_nodes and node != msprime.NULL_NODE:
                 added_nodes.add(node)
                 node = tree.get_parent(node)
-        for n in added_nodes:
+        for node in added_nodes:
             try:
-                SFS[tree.get_num_leaves(n)] += tree.get_branch_length(n)
+                SFS[tree.get_num_leaves(node)] += tree.get_branch_length(node)
             except ValueError:
-                if tree.get_parent(n) == msprime.NULL_NODE:
-                    #the root node will not have a branch length, and can be ignored anyway
-                    pass
-                else:
+                #can ignore root node (no branch length, mutations above it are fixed anyway) but not other nodes
+                if tree.get_parent(node) != msprime.NULL_NODE:
                     raise
         if check_method:
             SFS2 = collections.Counter()
