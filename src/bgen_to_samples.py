d4eaa9f9b24768bd81ff6e720394b045ebf93e3c
Yan Wong <yan@yanwong.me>
2018-06-22 19:19:28 +0100

    make bgen reader work using GCHr38 locs from 1000G

--- src/bgen_to_samples.py
+++ src/bgen_to_samples.py
@@ -1,149 +1,67 @@
 """
 Convert UK-biobank phased BGEN files to tsinfer sample input
 """
-
-import csv
-from datetime import datetime as dt
-import argparse
-
 import numpy as np
-from tqdm import tqdm, trange
-
 import tsinfer
-from bgen_reader import read_bgen
-"""We assume that although bgen_reader.read_bgen is no coded to work on phased BGEN files
-the results stored in the ['genotypes'] array can be sensibly interpreted as phased
-see https://github.com/limix/bgen-reader-py/issues/8
-"""
+import bgen_reader
+from tqdm import tqdm
 
-
-def main():
-    parser = argparse.ArgumentParser(
-        description=__doc__)
-    parser.add_argument("-chr", "--chromosome", type=int, default=19, help="Which chromosome number (integer)")
-    args = parser.parse_args()
-    chr = args.chromosome
-
-
-    ancestral_alleles = {}
-    for ref_alt_file, aa_allele_pos in {'ref':0, 'alt':1}.items():
-        with open("/well/mcvean/pkalbers/ancestral/aa.bi.{}.snv.1000G.chr{}.txt".format(ref_alt_file, chr), newline='') as aa_file:
-            print("Reading ancestral allele states for {} from {} (start time {})".format(ref_alt_file, aa_file.name, dt.now()))
-            row_count = sum(1 for row in aa_file)
-            dummy = aa_file.seek(0)
-            aa_reader = csv.DictReader(aa_file, delimiter=' ')
-            for row in tqdm(aa_reader, total=row_count):
-                ancestral_alleles[row['rsID']]=row['Alleles'].split(",")[aa_allele_pos].upper()
-    
-    
-    sample_data =  tsinfer.SampleData(path="chrom{}.samples".format(chr))
+for chr in range(20,21):
+    sd =  tsinfer.load("1000G_GRCh38/H_sap_chr{}.samples".format(chr))
     bgen_file = "/well/ukbb-wtchg/v2/haplotypes/ukb_hap_chr{}_v2.bgen".format(chr)
-    bgen = read_bgen(bgen_file, verbose=False, size = 200)
-    sample_df = bgen['samples']
-    print("Inputting sample names from {} (start time {})".format(chr, dt.now()))
-    for i in tqdm(range(len(sample_df))):
-        samp_name = sample_df.iat[i,0]
-        sample_data.add_sample({'name':samp_name+"a"})
-        sample_data.add_sample({'name':samp_name+"b"})
+    bgen = bgen_reader.read_bgen(bgen_file, verbose=False, size = 500)
     
+    GRCh38_1000G = [s['ID'] for s in sd.sites_metadata[:]]
+    GRCh37_UK_BB = [s for s in bgen['variants']['rsid']]
+    GRCh37_UK_BB_info = {k['rsid']:[i,k['allele_ids'].split(",")] for i,k in bgen['variants'].iterrows()}
     
+    s1000G = frozenset(GRCh38_1000G)
+    sUK_BB = frozenset(GRCh37_UK_BB)
+    intersection_order1000G = [x for x in GRCh38_1000G if x in sUK_BB]
+    intersection_orderUK_BB = [x for x in GRCh37_UK_BB if x in s1000G]
+    assert len(intersection_order1000G) == len(intersection_orderUK_BB)
     
+    print("Chromosome {}, {} SNPs shared between 1000G GRCh38 ({} with aa) and UK-BB GRCh37 ({} total). Shared order = {}".format(
+        chr, len(intersection_order1000G), len(GRCh38_1000G), len(GRCh37_UK_BB), 
+        intersection_order1000G == intersection_orderUK_BB))
     
-    ref_is_ancestral = alt_is_ancestral = 0
-    v_not_in_aa = []
-    a_not_in_aa = []
-    variant_df = bgen['variants']
-    derived_freq = {}
-    print("Inputting haplotype data from chromosome {} (start time {})".format(chr, dt.now()))
-    for idx in trange(len(bgen['genotype'])):
-        if variant_df.at[idx,'nalleles'] == 2:
-            rsID = variant_df.at[idx,'rsid']
-            pos = variant_df.at[idx,'pos']
-            alleles = variant_df.at[idx,'allele_ids'].upper().split(",")
-            assert len(alleles) == 2
-            if rsID not in ancestral_alleles:
-                v_not_in_aa.append(rsID)
-                try:
-                    sample_data.add_site(pos, alleles, np.ravel(bgen['genotype'][idx,:,0:2].compute(num_workers=40).astype(np.uint8)), inference=False)
-                except:
-                    debug_file="debug.npy"
-                    print("Error for pos {} (index {}) with alleles {}. Saving genotype data for debug to {}".format(pos, idx, alleles, debug_file))
-                    np.save(debug_file, np.ravel(bgen['genotype'][idx,:,0:2].compute(num_workers=40).astype(np.uint8)))
-                    raise
-            elif ancestral_alleles[rsID] not in alleles:
-                a_not_in_aa.append(rsID)
-                sample_data.add_site(pos, alleles, np.ravel(bgen['genotype'][idx,:,0:2].compute(num_workers=40).astype(np.uint8)), inference=False)
-            elif ancestral_alleles[rsID]==alleles[1]:
-                sample_data.add_site(pos, alleles[::-1], 1-np.ravel(bgen['genotype'][idx,:,0:2].compute(num_workers=40).astype(np.uint8)))
-                alt_is_ancestral += 1
-                derived_freq[rsID] = np.mean(1-bgen['genotype'][idx,:,0:2].compute(num_workers=40))
-            elif ancestral_alleles[rsID]==alleles[0]:
-                sample_data.add_site(pos, alleles, np.ravel(bgen['genotype'][idx,:,0:2].compute(num_workers=40).astype(np.uint8)))
-                ref_is_ancestral += 1
-                derived_freq[rsID] = np.mean(bgen['genotype'][idx,:,0:2].compute(num_workers=40))
-            else:
-                print("Unspecified error for {} on chr{} on line {} of {}".format(rsID, chr, idx, bgen_file))
-    
-    sample_data.finalise()
-    
-    ff_name = 'UKBB_freqs_chr{}.csv'.format(chr)
-    print("Writing frequencies to " + ff_name)
-    with open(ff_name,'w') as f:
-        for k,v in tqdm(derived_freq.items(), total=len(derived_freq)):
-            print("{},{}".format(k,v))
-    print("frequencies of derived alleles saved to freqs.csv")
-    print("{} variants marked NOT for inference ({} with no variant of that name, {} with no matching ancestral allele)".format(
-        len(v_not_in_aa) + len(a_not_in_aa), len(v_not_in_aa), len(a_not_in_aa)))
-    print("{} variants marked for inference ({} with ref ancestral, {} with alt ancestral)".format(
-        ref_is_ancestral + alt_is_ancestral, ref_is_ancestral, alt_is_ancestral))
-
-if __name__ == "__main__":
-    main()
-
-
-
-"""
-
-#To check frequencies
-
-from datetime import datetime as dt
-import csv
-
-from tqdm import tqdm, trange
-
-chr = 1
-ancestral_alleles = {}
-for ref_alt_file, aa_allele_pos in {'ref':0, 'alt':1}.items():
-    with open("/well/mcvean/pkalbers/ancestral/aa.bi.{}.snv.1000G.chr{}.txt".format(ref_alt_file, chr)) as aa_file:
-        print("Reading ancestral allele states for {} from {} (start time {})".format(ref_alt_file, aa_file.name, dt.now()))
-        row_count = sum(1 for row in aa_file)
-        dummy = aa_file.seek(0)
-        aa_reader = csv.DictReader(aa_file, delimiter=' ')
-        for row in tqdm(aa_reader, total=row_count):
-            ancestral_alleles[row['rsID']]=row['Alleles'].split(",")[aa_allele_pos].upper()
-
-derived_freq = {}
-num_samples = 0
-with open("/well/mcvean/ukbb12788/1000G/1000G.chr{}.marker.txt".format(chr)) as thousand_G:
-    row_count = sum(1 for row in thousand_G)
-    dummy = thousand_G.seek(0)
-    tg_reader = csv.DictReader(thousand_G, delimiter=' ')
-    for row in tqdm(tg_reader, total=row_count):
-        rsID = row['Label']
-        num_samples = max(num_samples, int(row['AlleleCount0']), int(row['AlleleCount0']), int(row['AlleleCountX']))
-        alleles = row['Alleles'].split(",")
-        if len(alleles) == 2:
-            if rsID in ancestral_alleles and ancestral_alleles[rsID] in alleles:
-                if ancestral_alleles[rsID] == alleles[0]:
-                    derived_freq[rsID] = int(row['AlleleCount1'])
-                elif ancestral_alleles[rsID] == alleles[1]:
-                    derived_freq[rsID] = int(row['AlleleCount0'])
+    with tsinfer.SampleData(path="UK_BB/UK-BB_chr{}.samples") as sample_data:
+        sample_df = bgen['samples']
+        for i in tqdm(range(len(sample_df)), desc="Read sample names"):
+            samp_name = sample_df.iat[i,0]
+            dummy = sample_data.add_individual(metadata={'name':samp_name}, ploidy=2)
+        
+        
+        ref_is_ancestral = alt_is_ancestral = alleles_differ = nomatch = 0
+        allele1_index = np.array([0], dtype=np.uint8) #the index of allele 1 in the UKBB allele list. 
+        allele2_index = np.array([1], dtype=np.uint8) #the index of allele 2 in the UKBB allele list
+        allele1_indices = np.array([0,2]) #the bgen indexes of the prob of allele1 in maternal & paternal chrs
+        
+        
+        for pos, md, alleles in tqdm(
+            zip(sd.sites_position[:], sd.sites_metadata[:], sd.sites_alleles[:]),
+            desc="Read bgen", total=len(bgen['genotype'])):
+            try:
+                idx, UKBBalleles = GRCh37_UK_BB_info[md['ID']]
+                if UKBBalleles==alleles:
+                    ref_is_ancestral += 1
+                    genotypes = np.where(
+                        #NB: np.ravel does this in the right way, [[1,2],[3,4],[5,6]] -> [1,2,3,4,5,6]
+                        np.ravel(bgen['genotype'][idx,:,:].compute(num_workers=40)[:,allele1_indices] > 0.5),
+                        allele1_index, allele2_index)
+                    dummy = sample_data.add_site(pos, genotypes, alleles)
+                elif list(reversed(UKBBalleles))==alleles:
+                    alt_is_ancestral += 1
+                    genotypes = np.where(
+                        np.ravel(bgen['genotype'][idx,:,:].compute(num_workers=40)[:,allele1_indices] > 0.5),
+                        allele2_index, allele1_index) #reverse alt & ref 
+                    dummy = sample_data.add_site(pos, genotypes, alleles)
                 else:
-                    assert False
-
-ff_name = '1000G_freqs_chr{}.csv'.format(chr)
-print("Writing frequencies to " + ff_name)
-with open(ff_name,'w') as f:
-    for k,v in tqdm(derived_freq.items(), total=len(derived_freq)):
-        print("{},{}".format(k,v/num_samples), file=f)
-"""
+                    alleles_differ += 1
+                    print("Alleles differ {}, {}".format(UKBBalleles,alleles))
+            except KeyError:
+                #can omit any rsIDs not in GRCh37_UK_BB_index
+                nomatch += 1
+    
+    print("Finished: {} with ref ancestral and {} with alt ancestral. {} with neither, and {} omitted".format(
+        ref_is_ancestral, alt_is_ancestral, alleles_differ, nomatch))
