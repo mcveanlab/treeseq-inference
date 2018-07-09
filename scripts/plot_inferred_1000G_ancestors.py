1f6a7d38e93fa410bb1b8ddf3fb16cd9e199f56b
Yan Wong <yan@yanwong.me>
2018-07-09 15:11:56 +0100

    Make nicer plots with full information

--- scripts/plot_inferred_1000G_ancestors.py
+++ scripts/plot_inferred_1000G_ancestors.py
@@ -23,6 +23,10 @@ def running_mean(x, N):
 parser = argparse.ArgumentParser(description='Plot ancestors generated from the 1000G data.')
 parser.add_argument('infile',
                     help='a path to the 1000G.samples file or 1000G.ancestors file. If a sample file, the ancestors file is created and saved')
+parser.add_argument('-ly', '--log-yscale', action='store_true'
+                    help='Should the y scale be logged')
+parser.add_argument('-cp', '--chromosome-positions', action='store_true'
+                    help='Should we plot the lengths in terms of chromosome positions')
 args = parser.parse_args()
 
 
@@ -36,20 +40,55 @@ if data.__class__ == tsinfer.formats.SampleData:
         progress_monitor=tsinfer.cli.ProgressMonitor(generate_ancestors=True))
 else:
     anc=data
+
 frequency = anc.ancestors_time[:]
-lengths = anc.ancestors_end[:]-anc.ancestors_start[:]
-
-df = pd.DataFrame(
-    {'l':lengths, 'f':frequency, 'nsites': [len(x) for x in anc.ancestors_focal_sites[:]]}
-    )
-
-lengths_by_anc_time = df.iloc[df['nsites'].nonzero()].groupby('f', sort=True).mean()
+positions = np.append(anc.sites_position[:], anc.sequence_length)
+lengths_by_sites = anc.ancestors_end[:]-anc.ancestors_start[:]
+lengths_by_pos = (positions[anc.ancestors_end[:]]-positions[anc.ancestors_start[:]])/1000
 
 
-plt.semilogy(lengths_by_anc_time.index, lengths_by_anc_time['l'], label="Estimated ancestors")
-plt.ylim(0, np.max(lengths_by_anc_time['l'][0:lengths_by_anc_time.shape[0]//2])) #set to max of younger ancestors (we have a few very long old ancestors
+df = pd.DataFrame({
+    'l':lengths_by_pos if args.chromosome_positions else lengths_by_sites,
+    'f':frequency, 
+    'nsites': [len(x) for x in anc.ancestors_focal_sites[:]]})
+
+df_all = pd.DataFrame(
+    {'lengths_per_site': np.repeat(df.l.values, df.nsites.values),
+    'f': np.repeat(df.f.values, df.nsites.values),
+    't':1
+    }).sort_values(by=['f'])
+df_all['pos'] = range(df_all.shape[0])
+df_all['mean_pos'] = np.repeat(df_all.groupby('f').mean().pos.values, df_all.groupby('f').sum().t.values)
+df_all['width'] = np.repeat(df_all.groupby('f').sum().t.values, df_all.groupby('f').sum().t.values)
+
+mean_by_anc_time = df.iloc[df['nsites'].nonzero()].groupby('f', sort=True).mean()
+median_by_anc_time = df.iloc[df['nsites'].nonzero()].groupby('f', sort=True).median()
+sum_by_anc_time = df.iloc[df['nsites'].nonzero()].groupby('f', sort=True).sum()
+
+fpos = np.insert(np.cumsum(sum_by_anc_time['nsites']).values, 0, 0)
+
+fig = plt.figure(figsize=(10,10), dpi=100)
+x_jittered = df_all.mean_pos.values+np.random.uniform(-df_all.width.values/3, df_all.width.values/3, len(df_all.mean_pos.values))
+#plot with jitter
+plt.scatter(x_jittered, df_all.lengths_per_site.values, marker='.', s=72./fig.dpi, alpha=0.05)
+plt.ylim(1, np.max(mean_by_anc_time['l'][0:mean_by_anc_time.shape[0]//2])*2) #set to max of younger ancestors (we have a few very long old ancestors
+if args.log_yscale:
+    plt.yscale("log")
+ax = plt.gca()
+ax.step(fpos[:-1], mean_by_anc_time['l'], label="Mean ancestor length", where='post', color="orange")
+ax.step(fpos[:-1], median_by_anc_time['l'], label="Median ancestor length", where='post', color="orange", linetype=":")
+ax.set_xlim(xmin=0)
+_ = ax.tick_params(axis='x', which="major", length=0)
+_ = ax.set_xticklabels('', minor=True)
+_ = ax.set_xticks(fpos[:-1], minor=True)
+_ = ax.set_xticks(fpos[:-1]+np.diff(fpos)/2)
+_ = ax.set_xticklabels(np.where(
+    np.isin(mean_by_anc_time.index, np.array([1,2,3,4,5,6,10,50,1000, 5000])), 
+    mean_by_anc_time.index,
+    ""))
 plt.xlabel("Ancestors_time (=freq, youngest to oldest)")
-plt.ylabel("Length")
+plt.ylabel("Length" + ("(kb)" if args.chromosome_positions else "(# sites)"))
 plt.legend()
-plt.savefig("{}_length-time.png".format(os.path.basename(fname)))
+plt.savefig("{}_{}_length-time.png".format(
+    os.path.basename(fname),"kb" if args.chromosome_positions else "sites"), dpi=100)
 plt.clf()
