599012e464348a9a199d8a2e9846e6d9b9177392
Jerome Kelleher <jk@well.ox.ac.uk>
2018-09-10 15:54:54 +0100

    Added Makefile to prepare the 1000 Genomes data.

--- human-data/convert_1kg.py
+++ human-data/convert_1kg.py
@@ -1,12 +1,11 @@
 """
-Example script for importing 1000 Genomes data.
+Script to import 1000 genomes data
 """
 import argparse
 import subprocess
 import os
 import shutil
 import sys
-import mmap
 
 import numpy as np
 import msprime
@@ -15,15 +14,7 @@ import attr
 import cyvcf2
 import tqdm
 
-def get_num_lines(file_path):
-    fp = open(file_path, "r+")
-    buf = mmap.mmap(fp.fileno(), 0)
-    lines = 0
-    while buf.readline():
-        lines += 1
-    return lines
-    
-    
+
 @attr.s()
 class Site(object):
     position = attr.ib(None)
@@ -94,55 +85,44 @@ def filter_duplicates(vcf):
     if row is not None and bad_pos != -1:
         yield row
 
+
 def vcf_num_rows(vcf_path):
-    """A quick way to get the number of sites in a vcf"""
-    try:
-        output = subprocess.check_output(["bcftools", "index", "--nrecords", vcf_path])
-    except subprocess.CalledProcessError:
-        #might be lacking an index
-        print("Creating an index file")
-        subprocess.call(["bcftools", "index", vcf_path])
-        output = subprocess.check_output(["bcftools", "index", "--nrecords", vcf_path])
+    """
+    Return the number of rows in the VCF. Requires an index to be present.
+    """
+    output = subprocess.check_output(["bcftools", "index", "--nrecords", vcf_path])
     return int(output)
 
 
-def variants(vcf_path, show_progress=False, ancestral_states=None, include_all_metadata=False):
+def variants(vcf_path, ancestral_states, show_progress=False, max_sites=None):
     """
-    Yield a tuple of position, alleles, genotypes, metadata
-    If ancestral_states is given, it should be a dictionary mapping name to state
+    Yield a tuple of position, alleles, genotypes, metadata. Ancestral_states is a
+    dictionary mapping RSID to the ancestral allele.
     """
     tot_sites = vcf_num_rows(vcf_path)
-    progress = tqdm.tqdm(total=tot_sites, desc="Read sites", disable=not show_progress)
+    progress = tqdm.tqdm(
+        total=tot_sites, desc="Read genotypes", disable=not show_progress)
 
     vcf = cyvcf2.VCF(vcf_path)
 
-    sites_used = non_biallelic = non_diploid = no_ancestral_state = missing_data = 0
+    sites_used = non_biallelic = non_diploid = no_ancestral_state = missing_data = no_rsid = 0
     num_diploids = len(vcf.samples)
     num_samples = 2 * num_diploids
     j = 0
     for row in filter_duplicates(vcf):
         progress.update()
         skip = False
-        ancestral_state = None
-        try:
-            if ancestral_states is not None:
-                aa = ancestral_states[row.ID]
-            else:
-                aa = row.INFO["AA"]
-            # Format: for indels = AA|REF|ALT|IndelType; for SNPs = AA
-            splits = aa.split("|")
-            if len(splits[0]) == 1:
-                base = splits[0].upper()
-                if base in "ACTG":
-                    ancestral_state = base
-        except KeyError:
-            pass
-        #only use biallelic sites with data for all samples, where ancestral state is known
-        if len(row.ALT)!=1:
-            non_biallelic += 1
-            skip = True
-        if ancestral_state is None:
+        # only use biallelic sites with data for all samples, where ancestral state is known
+        # and we have an RSID.
+        if row.ID is None:
+            no_rsid += 1
+        if row.ID in ancestral_states:
+            ancestral_state = ancestral_states[row.ID]
+        else:
             no_ancestral_state += 1
+            skip = True
+        if len(row.ALT) != 1:
+            non_biallelic += 1
             skip = True
         if row.num_called != num_diploids:
             missing_data += 1
@@ -162,22 +142,27 @@ def variants(vcf_path, show_progress=False, ancestral_states=None, include_all_m
             except IndexError:
                 print("Bad set of alleles: {} for sample {} at {} (ancestral state = {})".format(
                     bases[j], j, row.ID, ancestral_state))
-                continue 
+                continue
             if len(all_alleles) != 2:
                 non_diploid += 1
             else:
-                progress.set_postfix(infer_sites=sites_used)
+                progress.set_postfix(used=sites_used)
                 all_alleles.remove(ancestral_state)
                 alleles = [ancestral_state, all_alleles.pop()]
-                metadata = {"ID": row.ID, "INFO": dict(row.INFO)} if include_all_metadata else {"ID": row.ID}
+                metadata = {"ID": row.ID}
                 sites_used += 1
-                yield Site(
-                    position=row.POS, alleles=alleles, genotypes=a, metadata=metadata)
+                yield Site(position=row.POS, alleles=alleles, genotypes=a, metadata=metadata)
+                if max_sites == sites_used:
+                    break
+    progress.close()
     vcf.close()
-    print("Used {} out of {} sites. {} non biallelic, {} without ancestors, {} with missing data, and {} not diploid".format(
-        sites_used, tot_sites, non_biallelic, no_ancestral_state, missing_data, non_diploid))
+    print(
+        "Used {} out of {} sites. {} non biallelic, {} without rsid {} without ancestral state, "
+        "{} with missing data, and {} not diploid".format(
+        sites_used, tot_sites, non_biallelic, no_rsid, no_ancestral_state, missing_data,
+        non_diploid))
 
-def add_samples(ped_file, population_id_map, individual_names, sample_data, show_progress=False):
+def add_samples(ped_file, population_id_map, individual_names, sample_data):
     """
     Reads the specified PED file to get information about the samples.
     Assumes that the population IDs have already been allocated and
@@ -186,8 +171,7 @@ def add_samples(ped_file, population_id_map, individual_names, sample_data, show
     columns = next(ped_file).split("\t")
     sane_names = [col.replace(" ", "_").lower().strip() for col in columns]
     rows = {}
-    for line in tqdm.tqdm(ped_file, total=get_num_lines(ped_file.name), 
-        desc="Read population data", disable=not show_progress):
+    for line in ped_file:
         metadata = dict(zip(sane_names, line.strip().split("\t")))
         metadata["population"] = population_id_map[metadata["population"]]
         name = metadata["individual_id"]
@@ -201,48 +185,56 @@ def add_samples(ped_file, population_id_map, individual_names, sample_data, show
         rows[name] = nulled
 
     # Add in the metadata rows in the order of the VCF.
-    for name in tqdm.tqdm(individual_names, desc="Add sites", disable=not show_progress):
-        metadata = rows[name]
-        sample_data.add_individual(metadata=metadata, ploidy=2)    
+    for name in individual_names:
+        sample_data.add_individual(metadata=rows[name], ploidy=2)
 
-def convert(vcf_file, pedigree_file, output_file, 
-    max_variants=None, show_progress=False, ancestor_file=None, include_all_metadata=False):
+
+def read_ancestral_states(ancestor_file, show_progress=False):
+    states = {}
+    sites = vcf_num_rows(ancestor_file)
+    vcf = cyvcf2.VCF(ancestor_file)
+    iterator = tqdm.tqdm(
+        vcf, total=sites, desc="Read ancestral state", disable=not show_progress)
+    for site in iterator:
+        if site.ID is not None:
+            try:
+                aa = site.INFO["AA"]
+            except KeyError:
+                aa = None
+            if aa is not None:
+                # Format: for indels = AA|REF|ALT|IndelType; for SNPs = AA
+                splits = aa.split("|")
+                if len(splits[0]) == 1:
+                    base = splits[0].upper()
+                    if base in "ACTG":
+                        states[site.ID] = base
+    vcf.close()
+    print("Read {} ancestral alleles out of {} sites from {}".format(
+        len(states), sites, ancestor_file))
+    return states
+
+
+def convert(
+        vcf_file, ancestral_states_file, pedigree_file, output_file, max_variants=None,
+        show_progress=False):
 
     if max_variants is None:
         max_variants = 2**32  # Arbitrary, but > defined max for VCF
 
     sample_data = tsinfer.SampleData(path=output_file, num_flush_threads=2)
     pop_id_map = add_populations(sample_data)
-    
+
     vcf = cyvcf2.VCF(vcf_file)
     individual_names = list(vcf.samples)
     vcf.close()
 
     with open(pedigree_file, "r") as ped_file:
-        add_samples(ped_file, pop_id_map, individual_names, sample_data, show_progress)
+        add_samples(ped_file, pop_id_map, individual_names, sample_data)
 
-    if ancestor_file is not None:
-        ancestors = {}
-        sites=vcf_num_rows(ancestor_file)
-        vcf = cyvcf2.VCF(ancestor_file)
-        for site in tqdm.tqdm(
-            vcf, total=sites, desc="Read ancestor state", disable=not show_progress):
-            if site.ID:
-                try:
-                    ancestors[site.ID] = site.INFO["AA"]
-                except KeyError:
-                    pass
-        vcf.close()
-        if show_progress:
-            print("Read {} ancestral alleles out of {} sites from {}".format(
-                len(ancestors), sites, ancestor_file))
-    else:
-        ancestors = None
-
-    for index, site in enumerate(variants(vcf_file, show_progress, ancestors, include_all_metadata)):
+    ancestral_states = read_ancestral_states(ancestral_states_file, show_progress)
+    iterator = variants(vcf_file, ancestral_states, show_progress, max_variants)
+    for site in iterator:
         sample_data.add_site(site.position, site.genotypes, site.alleles, site.metadata)
-        if index == max_variants:
-            break
     sample_data.finalise(command=sys.argv[0], parameters=sys.argv[1:])
 
 
@@ -251,24 +243,22 @@ def main():
         description="Script to convert VCF files into tsinfer input.")
     parser.add_argument(
         "vcf_file",
-        help="The input VCF file. E.g. from http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/supporting/GRCh38_positions/")
+        help="The input VCF file.")
+    parser.add_argument(
+        "ancestral_states_file",
+        help="A vcf file containing ancestral allele states. ")
     parser.add_argument(
         "pedigree_file",
-        help="The pedigree file containing population and sample data, e.g. ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/working/20130606_sample_info/20130606_g1k.ped")
+        help="The pedigree file containing population and sample data")
     parser.add_argument(
         "output_file",
         help="The tsinfer output file")
-    parser.add_argument(
-        "-a", "--ancestors_file", default=None, 
-        help="A vcf file containing ancestral allele states. This will override ancestral alleles from the main vcf. E.g. ftp://ftp.ensembl.org/pub/release-91/variation/vcf/homo_sapiens/homo_sapiens.vcf.gz. To save time, you may want to split this into one file per chromosome, e.g. using `bcftools view homo_sapiens.vcf.gz --regions 1 -Oz -o homo_sapiens_chr1.vcf.gz`")
     parser.add_argument(
         "-n", "--max-variants", default=None, type=int,
         help="Keep only the first n variants")
     parser.add_argument(
         "-p", "--progress", action="store_true",
         help="Show progress bars and output extra information when done")
-    parser.add_argument(
-        "-m", "--include_all_metadata", action="store_true")
 
     args = parser.parse_args()
 
@@ -279,8 +269,8 @@ def main():
 
 
     convert(
-        args.vcf_file, args.pedigree_file, args.output_file, args.max_variants, 
-        show_progress=args.progress, ancestor_file = args.ancestors_file)
+        args.vcf_file, args.ancestral_states_file, args.pedigree_file, args.output_file,
+        args.max_variants, show_progress=args.progress)
 
 
 if __name__ == "__main__":
