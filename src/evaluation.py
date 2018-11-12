3b306bcfbab47a0944da52e541914811fc574d49
Yan Wong <yan@yanwong.me>
2018-11-12 23:50:44 +0000

    Only give equivalent error rate if error is used

--- src/evaluation.py
+++ src/evaluation.py
@@ -1037,7 +1037,7 @@ class Dataset(object):
                 position=v.site.position, alleles=v.alleles,
                 genotypes=genotypes)
 
-            if record_rate:
+            if record_rate and (error_param != 0):
                 logging.info(" actual error rate = {} over {} sites".format(
                     bits_flipped/(n_variants*ts.sample_size), n_variants))
 
